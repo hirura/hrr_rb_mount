6ed86716c5f651cc702e8ebf2ffb46fe21cd13a7
hirura <hirura@gmail.com>
2020-03-08 20:29:33 +0900

    update to remove unused Error class and to add documentation

--- lib/hrr_rb_mount.rb
+++ lib/hrr_rb_mount.rb
@@ -1,7 +1,7 @@
 require "hrr_rb_mount/version"
 require "hrr_rb_mount/hrr_rb_mount"
 
+# A wrapper around mount and umount.
+# See mount(2) and umount(2) for details.
 module HrrRbMount
-  class Error < StandardError; end
-  # Your code goes here...
 end
